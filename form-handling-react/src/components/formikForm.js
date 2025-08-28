541ce9c20242b19d5988b68dee3268160b93ade4
karen wambui <karenwambui705@gmail.com>
2025-08-28 17:47:44 +0300

    adjusted formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -24,4 +24,52 @@ export default function FormikForm() {
       });
 
       if (res.ok) {
-        alert("User regis
+        alert("User registered successfully!");
+        resetForm();
+      } else {
+        alert("Failed to register user.");
+      }
+    } catch (err) {
+      alert("Network error. Try again.");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "420px", margin: "24px auto", padding: 12 }}>
+      <h2>Registration Form (Formik + Yup)</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <Field type="text" name="username" placeholder="Username" />
+              <ErrorMessage name="username" component="p" style={{ color: "red" }} />
+            </div>
+            <br />
+
+            <div>
+              <Field type="email" name="email" placeholder="Email" />
+              <ErrorMessage name="email" component="p" style={{ color: "red" }} />
+            </div>
+            <br />
+
+            <div>
+              <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage name="password" component="p" style={{ color: "red" }} />
+            </div>
+            <br />
+
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Submitting..." : "Register"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
